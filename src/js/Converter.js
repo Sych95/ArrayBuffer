46824050e26b119ca8877f74b618e162dc83b30b
SergejBeschastnov <svb19.09@yandex.ru>
2021-11-26 10:21:42 +0300

    Converter corrected & .appveyor.yml added

--- src/js/Converter.js
+++ src/js/Converter.js
@@ -4,7 +4,7 @@ export class ArrayBufferConverter {
   }
 
   toString() {
-    const view = new Uint8Array(this.loadedBuffer);
+    const view = new Uint16Array(this.loadedBuffer);
     let stringArray = view.slice();
     stringArray = Array.from(stringArray);
     const string = stringArray.map((item) => { item = String.fromCharCode(item); return item; }).join('');
